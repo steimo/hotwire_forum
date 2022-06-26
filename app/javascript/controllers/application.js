9b0c052255ab387217e0f1aebb3c891e46c142e1
steimz <vladyslav.kolomiets@protonmail.com>
2022-06-26 21:23:11 +0300

    Add flash messages using tailwind-stimulus

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,7 +1,8 @@
 import { Application } from "@hotwired/stimulus"
-
+import { Alert } from "tailwindcss-stimulus-components"
 const application = Application.start()
 
+application.register('alert', Alert)
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
