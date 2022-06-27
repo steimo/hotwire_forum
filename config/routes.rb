235c1a6b9bca801ffd81b06cdce6d6f059ea946d
steimz <vladyslav.kolomiets@protonmail.com>
2022-06-27 12:21:10 +0300

    Add discussion partial and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'main/index'
+  resources :discussions, only: [:index]
   root 'main#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
