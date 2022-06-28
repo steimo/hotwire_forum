7815600de7f0261abea12c0e17a67c1c8d2c197a
steimz <vladyslav.kolomiets@protonmail.com>
2022-06-28 18:08:55 +0300

    Add edit action and view

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,5 +1,7 @@
 class DiscussionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_discussion, only: %i[edit update]
+
   def index
     @discussions = Discussion.all
   end
@@ -20,9 +22,25 @@ class DiscussionsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to discussions_path, notice: 'Discussion updated' }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def discussion_params
     params.require(:discussion).permit(:name, :closed, :pinned)
+  end
+
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
   end
 end
