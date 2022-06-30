07d8f6c2801f02e9f7a6f1f0b2d40f8273cd6246
steimz <vladyslav.kolomiets@protonmail.com>
2022-06-30 22:04:29 +0300

    Can create post from discussion show view

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -6,7 +6,9 @@ class DiscussionsController < ApplicationController
     @discussions = Discussion.all.order(updated_at: :desc)
   end
 
-  def show; end
+  def show
+    @new_post = @discussion.posts.new
+  end
 
   def new
     @discussion = Discussion.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :discussions, only: %i[index show new create edit update destroy]
+  resources :discussions, only: %i[index show new create edit update destroy] do 
+    resources :posts, only: [:create], module: :discussions
+  end
   root 'main#index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
