12485d87977a585e9ffec969cccfca0ba5cbc433
steimz <vladyslav.kolomiets@protonmail.com>
2022-07-02 22:49:38 +0300

    Add broadcast to posts on the discussion page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   has_rich_text :body
   validates :body, presence: true
+
+  after_create_commit -> { broadcast_append_to discussion, partial: 'discussions/posts/post', locals: { post: self } }
 end
