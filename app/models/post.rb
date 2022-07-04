f2777c1b689f0dba8bc7f998863531b575c735ad
steimz <vladyslav.kolomiets@protonmail.com>
2022-07-04 18:26:18 +0300

    Install noticed and add new post notification

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user, default: -> { Current.user }
   has_rich_text :body
   validates :body, presence: true
+  has_noticed_notifications
 
   after_create_commit -> { broadcast_append_to discussion, partial: 'discussions/posts/post', locals: { post: self } }
   after_update_commit -> { broadcast_replace_to discussion, partial: 'discussions/posts/post', locals: { post: self } }
